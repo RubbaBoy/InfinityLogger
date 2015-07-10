0a819d48a9a7021818e88afb94a411a8d83a374f
RubbaBoy <adamyarris@yahoo.com>
2015-07-10 14:53:52 -0400

    Create PlayerInvPickupItem.java

---
+++ src/com/rubbaboy/Listeners/PlayerInvPickupItem.java
@@ -0,0 +1,34 @@
+/*
+Copyright (C) 2015  Adam Yarris
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package com.rubbaboy.Listeners;
+
+import com.rubbaboy.log.Logger;
+import org.bukkit.Location;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerPickupItemEvent;
+
+public class PlayerInvPickupItem implements Listener {
+
+    @EventHandler
+    public void onItemPickup(PlayerPickupItemEvent event) throws Exception {
+        Location loc = event.getPlayer().getLocation();
+        Logger logger = new Logger();
+        logger.Log(5, "Dragged item in inventory with player coordinates at X:" + loc.getBlockX() + " Y:" + loc.getBlockY() + " Z:" + loc.getBlockZ() + " item picked up being type " + event.getItem().getItemStack().getType().toString() + " players gamemode was " + event.getPlayer().getGameMode().toString() + " player flying is " + event.getPlayer().isFlying(), event.getPlayer());
+    }
+}
